894f6aebe9cd219bcfcf744ab9afb35be529e878
rla28-ua <rla28@alu.ua.es>
2022-03-04 13:07:18 +0100

    Clases 'Equipo.cs' y 'Program.cs' empezadas

--- hada-p2/Equipo.cs
+++ hada-p2/Equipo.cs
@@ -4,9 +4,85 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace hada_p2
+namespace Hada
 {
-    internal class Equipo
+    class Equipo
     {
+        private List<Jugador> jugadores;
+        private List<Jugador> amonestacionesExc;
+        private List<Jugador> faltasExc;
+        private List<Jugador> puntosExc;
+        private List<Jugador> energiaExc;
+        public static int minJugadores { get; set; }
+        public static int maxNumeroMovimientos { get; set; }
+        public static int movimientos { get; private set; }
+        public static string nombreEquipo { get; private set; }
+
+        public Equipo(int nj, string nom)
+        {
+            jugadores = new List<Jugador>();
+            amonestacionesExc = new List<Jugador>();
+            faltasExc = new List<Jugador>();
+            puntosExc = new List<Jugador>();
+            energiaExc = new List<Jugador>();
+
+            for(int i = 0; i < nj; i++)
+            {
+                jugadores.Add(new Jugador("jugador_" + i, 0, 0, 0, 50));
+                jugadores[i].
+            }
+        }
+        public bool moverJugadores()
+        {
+            bool mover = false;
+
+            for(int i = 0; i < jugadores.Count(); i++)
+            {
+                if (jugadores[i].todoOk())
+                {
+                    mover = true;
+                    jugadores[i].mover();
+                }
+            }
+            return mover;
+        }
+        public void moverJugadoresEnBucle()
+        {
+            while (moverJugadores())
+            {
+                moverJugadores();
+            }
+        }
+        public int sumarPuntos()
+        {
+        }
+
+        public List<Jugador> getJugadoresExcedenLimiteAmonestaciones()
+        {
+            return amonestacionesExc;
+        }
+        public List<Jugador> getJugadoresExcedenLimiteFaltas()
+        {
+            return faltasExc;
+        }
+        public List<Jugador> getJugadoresExcedenMinimoEnergia()
+        {
+            return energiaExc;
+        }
+        public override string ToString()
+        {
+            string cadena;
+
+            cadena = 
+
+            return cadena;
+        }
+
+        private cuandoAmonestacionesMaximoExcedido(object sender, AmonestacionesMaximoExcedidoArgs evento)
+        {
+            jugadores jugador = (jugador)sender;
+
+
+        }
     }
 }
